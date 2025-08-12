07f35abf2de4ad7672e8d723a12627786661e585
CruelDuck <jakub.cechlovsky@gmail.com>
2025-08-12 23:22:18 +0200

    Update Filters.tsx

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -17,7 +17,7 @@ export default function Filters() {
 
   return (
     <div className="card p-4 sticky top-4">
-      <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-3">
+      <div className="grid sm:grid-cols-2 lg:grid-cols-6 gap-3">
         <select className="select" value={get('typ')} onChange={e=>setParam('typ', e.target.value)}>
           <option value="">Typ: vše</option>
           <option>Nabídka</option>
@@ -28,7 +28,8 @@ export default function Filters() {
           <option>Seno</option>
           <option>Sláma</option>
         </select>
-        <input className="input" placeholder="Kraj (např. Jihomoravský)" value={get('kraj')} onChange={e=>setParam('kraj', e.target.value)} />
+        <input className="input" placeholder="Kraj" value={get('kraj')} onChange={e=>setParam('kraj', e.target.value)} />
+        <input className="input" placeholder="Okres" value={get('okres')} onChange={e=>setParam('okres', e.target.value)} />
         <input className="input" placeholder="Rok sklizně" value={get('rok')} onChange={e=>setParam('rok', e.target.value)} />
         <select className="select" value={get('sort') || 'newest'} onChange={e=>setParam('sort', e.target.value)}>
           <option value="newest">Řazení: Nejnovější</option>
