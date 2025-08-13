031745626c367a7f9766f70525adfdeed5842c89
CruelDuck <jakub.cechlovsky@gmail.com>
2025-08-13 09:56:44 +0200

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="container-p py-10 space-y-3">
+      <h1 className="text-2xl font-semibold">Stránka nenalezena</h1>
+      <a href="/" className="btn">Zpět na úvod</a>
+    </div>
+  )
+}
