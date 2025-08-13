e78798054bc8924ce8b235a08d605cfa46484217
CruelDuck <jakub.cechlovsky@gmail.com>
2025-08-13 08:46:30 +0200

    Update cz.ts

--- lib/cz.ts
+++ lib/cz.ts
@@ -19,38 +19,18 @@ export type Kraj = typeof KRAJE[number]
 
 export const OKRESY: Record<Kraj, string[]> = {
   'Hlavní město Praha': ['Praha'],
-
-  'Středočeský': [
-    'Benešov','Beroun','Kladno','Kolín','Kutná Hora','Mělník','Mladá Boleslav',
-    'Nymburk','Praha-východ','Praha-západ','Příbram','Rakovník'
-  ],
-
-  'Jihočeský': [
-    'České Budějovice','Český Krumlov','Jindřichův Hradec','Písek','Prachatice','Strakonice','Tábor'
-  ],
-
-  'Plzeňský': [
-    'Domažlice','Klatovy','Plzeň-město','Plzeň-jih','Plzeň-sever','Rokycany','Tachov'
-  ],
-
+  'Středočeský': ['Benešov','Beroun','Kladno','Kolín','Kutná Hora','Mělník','Mladá Boleslav','Nymburk','Praha-východ','Praha-západ','Příbram','Rakovník'],
+  'Jihočeský': ['České Budějovice','Český Krumlov','Jindřichův Hradec','Písek','Prachatice','Strakonice','Tábor'],
+  'Plzeňský': ['Domažlice','Klatovy','Plzeň-město','Plzeň-jih','Plzeň-sever','Rokycany','Tachov'],
   'Karlovarský': ['Cheb','Karlovy Vary','Sokolov'],
-
   'Ústecký': ['Děčín','Chomutov','Litoměřice','Louny','Most','Teplice','Ústí nad Labem'],
-
   'Liberecký': ['Česká Lípa','Jablonec nad Nisou','Liberec','Semily'],
-
   'Královéhradecký': ['Hradec Králové','Jičín','Náchod','Rychnov nad Kněžnou','Trutnov'],
-
   'Pardubický': ['Chrudim','Pardubice','Svitavy','Ústí nad Orlicí'],
-
   'Vysočina': ['Havlíčkův Brod','Jihlava','Pelhřimov','Třebíč','Žďár nad Sázavou'],
-
   'Jihomoravský': ['Blansko','Brno-město','Brno-venkov','Břeclav','Hodonín','Vyškov','Znojmo'],
-
   'Olomoucký': ['Jeseník','Olomouc','Prostějov','Přerov','Šumperk'],
-
   'Zlínský': ['Kroměříž','Uherské Hradiště','Vsetín','Zlín'],
-
   'Moravskoslezský': ['Bruntál','Frýdek-Místek','Karviná','Nový Jičín','Opava','Ostrava-město'],
 }
 
