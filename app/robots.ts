5ade921b1794d6e2d026d35d63b46c456e386d07
CruelDuck <jakub.cechlovsky@gmail.com>
2025-08-13 09:43:10 +0200

    Update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -1,9 +1,8 @@
 import type { MetadataRoute } from 'next'
-
 export default function robots(): MetadataRoute.Robots {
-  const base = process.env.NEXT_PUBLIC_SITE_URL || 'https://seno-slama.vercel.app'
+  const base = process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
   return {
     rules: [{ userAgent: '*', allow: '/' }],
-    sitemap: `${base}/sitemap.xml`
+    sitemap: `${base}/sitemap.xml`,
   }
 }
